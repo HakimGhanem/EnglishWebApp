ebd4ba3ac0cc6b59e9af60961ae120e80fe78bb6
chouder <chouderrayhan@yahoo.fr>
2017-01-26 14:21:29 +0100

    Ez

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Mon super site</title>
+    <title>Apprendre l'anglais en s'amusant</title>
 </head>
 
 <body>
@@ -18,8 +18,7 @@
     <!-- Le corps -->
     
     <div id="corps">
-        <h1>Mon super site</h1>
-        <h2>Mon super site h2</h2>
+        <h1>Apprendre l'anglais en s'amusant</h1>
         
         <p>
             Bienvenue sur mon super site !<br />

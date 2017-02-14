f1e23a45666c2c449ec54231353fc55d86293493
chouder3u <sofian.chouder@telecomnancy.net>
2017-02-14 15:34:16 +0100

    changement banniere

---
+++ quizztest.php
@@ -0,0 +1,89 @@
+<form action=""> 
+<div align="center"></div> 
+<table width="80%" border="0" cellspacing="3" cellpadding="0" bgcolor="#000099" align="center"> 
+<tr>  
+<td align="center" valign="middle">  
+<p>&nbsp;</p> 
+<table width="95%" border="0" cellspacing="3" cellpadding="0" bgcolor="#FFFFCC"> 
+<tr>  
+<td colspan="4">  
+<p align="center"><font face="Arial, Helvetica, sans-serif">Voulez-vous  
+vous tester en passant un  
+petit QCM ?</font></p> 
+<p>&nbsp;</p> 
+</td> 
+</tr> 
+<tr>  
+<td width="60%"><font face="Arial, Helvetica, sans-serif"></font></td> 
+<td width="10%">  
+<div align="center"><font face="Arial, Helvetica, sans-serif">OUI</font></div> 
+</td> 
+<td width="10%">  
+<div align="center"><font face="Arial, Helvetica, sans-serif">NON</font></div> 
+</td> 
+<td> </td> 
+</tr> 
+<tr>  
+<td width="60%"><font face="Arial, Helvetica, sans-serif">Question  
+0</font></td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton0" value="v0"> 
+</td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton0" value="v1"> 
+</td> 
+<td align="center">  
+<input type="text" name="textfield"> 
+</td> 
+</tr> 
+<tr>  
+<td width="60%"><font face="Arial, Helvetica, sans-serif">Question  
+1</font></td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton1" value="v0"> 
+</td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton1" value="v1"> 
+</td> 
+<td align="center">  
+<input type="text" name="textfield"> 
+</td> 
+</tr> 
+<tr>  
+<td width="60%"><font face="Arial, Helvetica, sans-serif">Question  
+2</font></td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton2" value="v0"> 
+</td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton2" value="v1"> 
+</td> 
+<td align="center">  
+<input type="text" name="textfield"> 
+</td> 
+</tr> 
+<tr>  
+<td width="60%"><font face="Arial, Helvetica, sans-serif">Question  
+3</font></td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton3" value="v0"> 
+</td> 
+<td width="10%" align="center">  
+<input type="radio" name="radiobutton3" value="v1"> 
+</td> 
+<td align="center">  
+<input type="text" name="textfield"> 
+</td> 
+</tr> 
+</table> 
+</td> 
+</tr> 
+<tr>  
+<td align="center" height="74" valign="middle">  
+<input type="button" name="Submit2" value="Voir mon r&eacute;sultat" onClick="valideReponse(this.form,[1,0,1,0])"> 
+<img src="../../imagesSite/transparent.gif" width="8" height="8">  
+<input type="reset" name="Submit2" value="Recommencer" onClick="reset();clearScreen(this.form);"> 
+</td> 
+</tr> 
+</table> 
+</form>
